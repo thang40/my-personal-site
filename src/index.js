29a7b72eb7b729280edb4c4396780c7b6bf02b7e
thang <thangle1192@gmail.com>
2019-09-19 16:45:43 +0700

    temporary remove airtable service

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { initService } from "./services/airtable.service";
 // Init airtable service
-initService();
+// initService();
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
